06255763511e51df694ab5a3d62bb7993a8c3ace
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:06:18 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "sentinel_data_connector_office_power_bis" {
+  description = "All sentinel_data_connector_office_power_bi resources"
+  value       = azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis
+}
+
