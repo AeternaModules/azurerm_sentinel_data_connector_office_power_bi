f48b092809705e0c38a3509b970cae34ff8c4280
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "sentinel_data_connector_office_power_bis" {
-  description = "All sentinel_data_connector_office_power_bi resources"
-  value       = azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis
-}
 output "sentinel_data_connector_office_power_bis_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_office_power_bis"
-  value       = [for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_office_power_bis, keyed the same as var.sentinel_data_connector_office_power_bis"
+  value       = { for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : k => v.log_analytics_workspace_id }
 }
 output "sentinel_data_connector_office_power_bis_name" {
-  description = "List of name values across all sentinel_data_connector_office_power_bis"
-  value       = [for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : v.name]
+  description = "Map of name values across all sentinel_data_connector_office_power_bis, keyed the same as var.sentinel_data_connector_office_power_bis"
+  value       = { for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : k => v.name }
 }
 output "sentinel_data_connector_office_power_bis_tenant_id" {
-  description = "List of tenant_id values across all sentinel_data_connector_office_power_bis"
-  value       = [for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : v.tenant_id]
+  description = "Map of tenant_id values across all sentinel_data_connector_office_power_bis, keyed the same as var.sentinel_data_connector_office_power_bis"
+  value       = { for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : k => v.tenant_id }
 }
 
