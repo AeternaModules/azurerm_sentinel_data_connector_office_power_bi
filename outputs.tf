74d8a511dd0411ace53d624e20210fdbd18c9aa0
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "sentinel_data_connector_office_power_bis_id" {
   description = "Map of id values across all sentinel_data_connector_office_power_bis, keyed the same as var.sentinel_data_connector_office_power_bis"
-  value       = { for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : k => v.id }
+  value       = { for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "sentinel_data_connector_office_power_bis_log_analytics_workspace_id" {
   description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_office_power_bis, keyed the same as var.sentinel_data_connector_office_power_bis"
-  value       = { for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : k => v.log_analytics_workspace_id }
+  value       = { for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : k => v.log_analytics_workspace_id if v.log_analytics_workspace_id != null && length(v.log_analytics_workspace_id) > 0 }
 }
 output "sentinel_data_connector_office_power_bis_name" {
   description = "Map of name values across all sentinel_data_connector_office_power_bis, keyed the same as var.sentinel_data_connector_office_power_bis"
-  value       = { for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : k => v.name }
+  value       = { for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "sentinel_data_connector_office_power_bis_tenant_id" {
   description = "Map of tenant_id values across all sentinel_data_connector_office_power_bis, keyed the same as var.sentinel_data_connector_office_power_bis"
-  value       = { for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : k => v.tenant_id }
+  value       = { for k, v in azurerm_sentinel_data_connector_office_power_bi.sentinel_data_connector_office_power_bis : k => v.tenant_id if v.tenant_id != null && length(v.tenant_id) > 0 }
 }
 
